4e5a9d1c8aa46149470d848983b1cb9945f56c30
Dmitriy Dubson <d.dubson@gmail.com>
2019-01-18 12:21:01 -0500

    perf(application): Update to use runApplication extension func

--- src/main/kotlin/com/ddubson/versioning/Application.kt
+++ src/main/kotlin/com/ddubson/versioning/Application.kt
@@ -2,8 +2,8 @@ package com.ddubson.versioning
 
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Application: ApplicationRunner {
@@ -16,5 +16,5 @@ class Application: ApplicationRunner {
 }
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    runApplication<Application>(*args)
 }
